8013268138dbba09cd68b5c7a15050c83c827bc1
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-10-19 18:07:56 -0400

    updates results cache function

--- build-assigment-objs-function.js
+++ build-assigment-objs-function.js
@@ -1,30 +1,80 @@
 import { learner1array, learner2array } from "./parse-learners-function.js";
 import { assigmentsArray } from "./parse-assigments-function.js"
 
-//console.log(learner2array)
 
 const objArr = []
 
-for (let i = 0; i<assigmentsArray.length; i++){
+// for (let i = 0; i<assigmentsArray.length; i++){
 
-const assignmentObj = {};
+// const assignmentObj = {};
 
-assignmentObj.id = assigmentsArray[i].id;
-assignmentObj.points_possible = assigmentsArray[i].points_possible
-assignmentObj.due_at = assigmentsArray[i].due_at;
-assignmentObj.learner1Id = learner1array[0].learner_id;
-assignmentObj.learner1score = learner1array[i].submission.score;
-assignmentObj.learner1submitted = learner1array[i].submission.submitted_at;
-assignmentObj.learner2Id = learner2array[0].learner_id;
-assignmentObj.learner2score = learner2array[i]?.submission.score;
-assignmentObj.learner2submitted = learner2array[i]?.submission.submitted_at;
-const points_possible = assigmentsArray[i].points_possible;
-assignmentObj.learner1avg = (learner1array[i].submission.score / points_possible ) * 100;
-assignmentObj.learner2avg = (learner2array[i]?.submission.score / points_possible ) * 100;
-objArr.push(assignmentObj)
+// assignmentObj.id = assigmentsArray[i].id;
+// assignmentObj.points_possible = assigmentsArray[i].points_possible
+// assignmentObj.due_at = assigmentsArray[i].due_at.split('-').join('');
+// assignmentObj.learner1Id = learner1array[0].learner_id;
+// assignmentObj.learner1score = learner1array[i].submission.score;
+// assignmentObj.learner1submitted = learner1array[i].submission.submitted_at.split('-').join('');
+// assignmentObj.learner2Id = learner2array[0].learner_id;
+// assignmentObj.learner2score = learner2array[i]?.submission.score;
+// assignmentObj.learner2submitted = learner2array[i]?.submission.submitted_at.split('-').join('');
+// const points_possible = assigmentsArray[i].points_possible;
+// assignmentObj.learner1avg = (learner1array[i].submission.score / points_possible ) * 100;
+// assignmentObj.learner2avg = (learner2array[i]?.submission.score / points_possible ) * 100;
+// objArr.push(assignmentObj)
+// }
+
+// function buildAssignmentObject(learnerArray1, learnerArray2, assignment_group){
+// }
+
+
+
+for (let i = 0; i < learner1array.length; i++) {
+
+    const assignmentObj1 = {};
+
+
+    const points_possible = assigmentsArray[i].points_possible;
+
+    assignmentObj1.id = assigmentsArray[i].id;
+    assignmentObj1.points_possible = assigmentsArray[i].points_possible
+    assignmentObj1.due_at = assigmentsArray[i].due_at.split('-').join('');
+    assignmentObj1.learner1Id = learner1array[0].learner_id;
+    assignmentObj1.learner1score = learner1array[i].submission.score;
+    assignmentObj1.learner1avg = (learner1array[i].submission.score / points_possible) * 100;
+    assignmentObj1.learner1submitted = learner1array[i].submission.submitted_at.split('-').join('');
+
+
+    objArr.push(assignmentObj1)
 
 }
 
-//console.log(objArr)
+for (let i = 0; i < learner2array.length; i++) {
+
+    const assignmentObj2 = {};
+
+    const points_possible = assigmentsArray[i].points_possible;
+
+    assignmentObj2.id = assigmentsArray[i].id;
+    assignmentObj2.points_possible = assigmentsArray[i].points_possible
+    assignmentObj2.due_at = assigmentsArray[i].due_at.split('-').join('');
+    assignmentObj2.learner2Id = learner2array[0].learner_id;
+    assignmentObj2.learner2score = learner2array[i].submission.score;
+    assignmentObj2.learner2submitted = learner2array[i].submission.submitted_at.split('-').join('');
+    assignmentObj2.learner2avg = (learner2array[i].submission.score / points_possible) * 100;
+
+    objArr.push(assignmentObj2)
+
+
+}
+
+//function to get total scores and total possible points
+
+//const total_points = objArr.map(el=>el.points_possible).reduce((a,b)=>a+b)
+// const learner1totalAvg = objArr.filter( el=>el.learner1Id === 125 ).map(el => el.learner1score).reduce((a, b) => a + b)
+
+
+
+// console.log(objArr)
+// console.log(total_points)
 
 export { objArr }
--- build-result-array.js
+++ build-result-array.js
@@ -1,47 +1,44 @@
 import { objArr } from "./build-assigment-objs-function.js";
 import { learner1array, learner2array } from "./parse-learners-function.js";
 
-//console.log(objArr)
-//console.log(learner1array)
-
-//const resultsArray = []
-
-
 //get possible points
 const possiblePointsSum = objArr.map((el)=>el.points_possible).reduce((a,b)=>a+b)
 
 //get learner 1 total score
-const learner1ScoreSum = objArr.map((el) => el.learner1score).reduce((a, b) => a + b)
+// const learner1ScoreSum = objArr.map((el) => el.learner1score).reduce((a, b) => a + b)
+const learner1ScoreSum = objArr.filter(el => el.learner1Id === 125).map(el => el.learner1score).reduce((a, b) => a + b)
+
+
 //get learner 1 avg score
 const learner1avg = (learner1ScoreSum / possiblePointsSum) * 100;
 
 
 //get learner 2 total score
-const learner2ScoreSum = objArr.map((el) => el.learner2score).reduce((a, b) => a + b)
-console.log(learner1ScoreSum)
+const learner2ScoreSum = objArr.filter(el => el.learner2Id === 132).map((el) => el.learner2score).reduce((a, b) => a + b);
 //get learner 2 avg score
 const learner2avg = (learner2ScoreSum / possiblePointsSum) * 100;
 
-//console.log(learner2avg)
-
+//console.log(objArr)
+console.log(learner1ScoreSum)
 
 const learner1resultsObj = {
     id: learner1array[0].learner_id,
     avg: learner1avg,
-    1: objArr[0].learner1avg,
-    2: objArr[1].learner1avg,
-    3: objArr[2]?.learner1avg,
+     1: objArr[0].learner1avg,
+     2: objArr[1].learner1avg,
+     3: objArr[2].learner1avg,
 }
 
 const learner2resultsObj = {
     id: learner2array[0].learner_id,
     avg: learner2avg,
-    1: objArr[0].learner2avg,
-    2: objArr[1].learner2avg,
-    // 3: objArr[2]?.learner2av
+    1: objArr[3].learner2avg,
+    2: objArr[4].learner2avg,
+    3: objArr[5]?.learner2avg,
 }
 
+const results = []
+results.push(learner1resultsObj, learner2resultsObj)
 
-//console.log(learner2array)
-// console.log(learner1resultsObj)
-// console.log(learner2resultsObj)
+
+console.log(results)
